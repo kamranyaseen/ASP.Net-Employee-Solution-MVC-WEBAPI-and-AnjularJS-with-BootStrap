9ba138f6e765110a3fb84742f5c75c92267bdf34
kamranmyaseen <kamranmyaseen@gmail.com>
2016-01-26 01:39:03 +0300

    Angularjs-ASPNET-WebAPI-BootStrap
    Employee CRUD Operation sample with Angularjs-ASPNET-WebAPI-BootStrap

---
+++ Angularjs-ASPNET-WebAPI-BootStrap/MVC_Using_Angular/MyScripts/AddEmpController.js
@@ -0,0 +1,26 @@
+﻿app.controller('AddEmployeeController', function ($scope, SinglePageCRUDService) {
+    $scope.EmpNo = 0;
+    $scope.save = function () {
+        var Employee = {
+            EmpNo: $scope.EmpNo,
+            EmpName: $scope.EmpName,
+            Salary: $scope.Salary,
+            DeptName: $scope.DeptName,
+            Designation: $scope.Designation
+        };
+     
+        var promisePost = SinglePageCRUDService.post(Employee);
+
+
+        promisePost.then(function (pl) {
+            $scope.EmpNo = pl.data.EmpNo;
+            alert("Successfully Saved. EmpNo " + pl.data.EmpNo);
+            $location.path("/showemployees");
+        },
+              function (errorPl) {
+                  $scope.error = 'failure loading Employee', errorPl;
+              });
+
+    };
+
+});
